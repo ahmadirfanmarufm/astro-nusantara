80ade8e8ed0b97f507401cc28e0b8e2dcd404291
Ahmad Irfan Ma'ruf Maulana <ahmadirfanxx28@gmail.com>
2024-06-22 15:25:37 +0700

    Mengganti nama projek

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import logo from '../../Logo-Astro-Nusantara.png';
+import logo from '../../Logo-Khodam-Primbon.png';
 
 const Home = () => {
     return (    
         <main className='container flex flex-col justify-center items-center '>
             <div className='bg-white p-5 rounded-md max-w-md mx-auto'>
                 <img src={logo} className='w-25 h-25 rounded-md mb-3' alt="Logo Astro Nusantara"/>
-                <p className='text-black mb-4'>Cari tahu penasaran dengan khodam, primbon, dan ramalan jawa di satu web</p>
+                <p className='text-black mb-4'>Cari tahu khodam, primbon, dan ramalan jawa di satu web</p>
                 <h1 className='text-black font-bold mb-4 text-xl'>PILIH APA YANG INGIN KAMU CEK</h1>
                 <div className='justify-center items-center flex flex-row gap-4 mb-3'>
                     <button type='button' className='bg-blue-500 shadow-lg shadow-blue-500/50 py-2 px-4 font-bold text-white rounded-md'>
